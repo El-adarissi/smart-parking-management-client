ba3d367adedf736032abda5bfa7089ad15c11e47
El-ADARISSI ABDELAZIZ <119929015+El-adarissi@users.noreply.github.com>
2025-04-03 11:03:15 +0000

    Update httpClient.js

--- src/authentification/httpClient.js
+++ src/authentification/httpClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    // baseURL: "https://smartparking-production.up.railway.app",
-    baseURL: "http://127.0.0.1:5000",
+    baseURL: "https://web-production-ee3f.up.railway.app",
+    // baseURL: "http://127.0.0.1:5000",
     withCredentials: true,  
     headers: {
         "Content-Type": "application/json"
