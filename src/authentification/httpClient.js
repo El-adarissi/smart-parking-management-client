a19ee50deed4c8607f36871fbaa027f9fae0bde6
El-ADARISSI ABDELAZIZ <119929015+El-adarissi@users.noreply.github.com>
2025-05-17 15:40:43 +0100

    Update httpClient.js

--- src/authentification/httpClient.js
+++ src/authentification/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: "https://web-production-ee3f.up.railway.app",
+    baseURL: "https://flask-hello-world-six-bay-54.vercel.app",
     // baseURL: "http://127.0.0.1:5000",
     withCredentials: true,  
     headers: {
